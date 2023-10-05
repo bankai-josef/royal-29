dadfcd7349baf00181925b1981552fc03f576bef
bankai-josef <145619872+bankai-josef@users.noreply.github.com>
2023-10-06 00:15:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿// Sélectionnez tous les boutons de menu
+const menuButtons = document.querySelectorAll('.menu-button');
+
+// Sélectionnez tous les éléments de menu
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Ajoutez un gestionnaire d'événements à chaque bouton de menu
+menuButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const targetId = button.getAttribute('data-target');
+        
+        // Cachez tous les éléments de menu
+        menuItems.forEach(item => {
+            item.style.display = 'none';
+        });
+        
+        // Affichez l'élément de menu cible
+        const targetItem = document.getElementById(targetId);
+        targetItem.style.display = 'block';
+    });
+});
